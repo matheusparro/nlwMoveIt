f6654706e4e7732a7249ce589d063f1d4970be36
matheusparro <mathparro@gmail.com>
2022-03-02 08:49:47 -0300

    Teste

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -2,6 +2,7 @@ import { useContext } from 'react'
 import { ChallengesContext } from '../contexts/ChallengesContext'
 import { CountdownContext } from '../contexts/CountdownContext'
 import styles from '../styles/components/ChallengeBox.module.css'
+//asdasdsada
 export function ChalllengeBox () {
   const {activeChallenge,resetChallenge,completeChallenge} = useContext(ChallengesContext)
   const { resetCountdown } = useContext(CountdownContext)
